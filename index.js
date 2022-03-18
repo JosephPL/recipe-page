f3f379b3b4cc505cc97230c189b121f7d7543c99
JosephPL <josephpl@outlook.com>
2022-03-18 14:42:07 -0600

    revision 1

--- index.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const port = 5000;
-const app = express();
-
-app.use(express.static(__dirname + '/public'));
-
-app.listen(port, ()=>{
-    console.log('funcionando en puerto ' + port);
-});
